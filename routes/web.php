24170a88df870f4df63161fac368c35b5af332c1
cutieziaa <kotaklaten732@gmail.com>
2025-11-25 09:53:44 +0700

    feat: Modify Route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 // Root SPA: semua route diarahkan ke React
-Route::get('/{any}', function () {
-    return view('app'); // ini adalah wrapper React (resources/views/app.blade.php)
-})->where('any', '.*');
+Route::get('{any}', function () {
+    return view('app');
+})->where('any', '^(?!api).*$');

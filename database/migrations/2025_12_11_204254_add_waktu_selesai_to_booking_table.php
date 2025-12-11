3f32185cdd6b808d36bfcfc65ff9fa900fc77c61
cutieziaa <kotaklaten732@gmail.com>
2025-12-12 04:40:22 +0700

    feat: modify booking

---
+++ database/migrations/2025_12_11_204254_add_waktu_selesai_to_booking_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('booking', function (Blueprint $table) {
+            // Menambahkan kolom waktu_selesai setelah kolom 'tanggal'
+            $table->dateTime('waktu_selesai')->nullable()->after('tanggal');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('booking', function (Blueprint $table) {
+            $table->dropColumn('waktu_selesai');
+        });
+    }
+};
